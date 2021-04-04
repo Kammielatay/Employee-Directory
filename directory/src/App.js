dd4f37e997852f66963a2e6cc9f74f3b050a535e
kammielatay <kameshialatay@gmail.com>
2021-04-04 12:29:10 -0700

    adding component files

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Search from "./components/Search";
+import Users from "./components/Users";
+import UserCard from "./components/UserCard";
 
 function App() {
   return (
     <div>
       <Header />
+      <Search />
+      <Users />
+      <UserCard />
     </div>
   );
 }
---
+++ directory/src/components/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+import API from "../utils/API";
+
+class Users extends React.Component{
+  state = {
+    users: []
+  }
+};
+
