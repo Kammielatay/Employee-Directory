19244e9a8a19fc68179f1e9af979b4f2456b5de0
kammielatay <kameshialatay@gmail.com>
2021-04-18 20:50:37 -0700

    changing file structure for better readability

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-import Search from "./components/SearchForm";
-//import Users from "./components/SearchResults";
-import Users from "./pages";
 
 function App() {
   return (
     <div>
       <Header />
-    
-      
+  
     </div>
   );
 }
